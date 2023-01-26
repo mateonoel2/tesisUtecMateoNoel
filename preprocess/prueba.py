6ada9054cc3236cf924131fd736f48833eec8f19
unknown <mateo.noel@utec.edu.pe>
2023-01-25 20:34:30 -0500

    hola

--- preprocess/prueba.py
+++ preprocess/prueba.py
@@ -3,8 +3,18 @@ import pandas as pd
 
 df = pd.read_csv("../datasets/MTA-Bus-Time_.2014-08-01.txt", sep="\t")
 
+map_vehicles_by_id =  {}
+max_vehicles = 10
+cont_num_vehicles = 0
+
 for index, row in df.iterrows():
     currentVehicleId = row["vehicle_id"]
-    subDf = df[df["vehicle_id"] == currentVehicleId]
-    print(subDf["next_scheduled_stop_id"])
-    break
+    if currentVehicleId != None and map_vehicles_by_id.get(currentVehicleId) == None:
+        subDf = df[df["vehicle_id"] == currentVehicleId]
+        map_vehicles_by_id[currentVehicleId] = subDf["next_scheduled_stop_id", "next_scheduled_stop_distance"]
+        df.drop(subDf.index, inplace=True)
+        cont_num_vehicles += 1
+    if cont_num_vehicles == max_vehicles:
+        break
+
+print(map_vehicles_by_id[469])
