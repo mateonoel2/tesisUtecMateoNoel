b0d835458ba6e509e97bbe8c6c24bce0df652546
unknown <mateo.noel@utec.edu.pe>
2023-01-25 21:17:44 -0500

    separar buses

--- preprocess/prueba.py
+++ preprocess/prueba.py
@@ -4,14 +4,16 @@ import pandas as pd
 df = pd.read_csv("../datasets/MTA-Bus-Time_.2014-08-01.txt", sep="\t")
 
 map_vehicles_by_id =  {}
-max_vehicles = 10
+max_vehicles = 1
 cont_num_vehicles = 0
+
+df.drop(df[df["inferred_phase"] == "LAYOVER_DURING"].index, inplace=True)
 
 for index, row in df.iterrows():
     currentVehicleId = row["vehicle_id"]
     if currentVehicleId != None and map_vehicles_by_id.get(currentVehicleId) == None:
         subDf = df[df["vehicle_id"] == currentVehicleId]
-        map_vehicles_by_id[currentVehicleId] = subDf["next_scheduled_stop_id", "next_scheduled_stop_distance"]
+        map_vehicles_by_id[currentVehicleId] = subDf[["time_received", "distance_along_trip", "next_scheduled_stop_distance", "next_scheduled_stop_id"]]
         df.drop(subDf.index, inplace=True)
         cont_num_vehicles += 1
     if cont_num_vehicles == max_vehicles:
