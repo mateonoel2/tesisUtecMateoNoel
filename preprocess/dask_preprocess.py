a724e5612a0541745354137bc8b6d07592350996
Mateo Noel <mateo.noel@utec.edu.pe>
2023-04-25 14:48:26 -0500

    Mini optimización

--- preprocess/dask_preprocess.py
+++ preprocess/dask_preprocess.py
@@ -3,50 +3,46 @@ import os
 from calc_data import calc_data
 import pandas as pd
 
-folder = "../datasets"
-datasets = os.listdir(folder)
-
-# Load your Dask DataFrame
-dataset_name = os.path.join(folder, datasets[0])
-ddf = dd.read_csv(dataset_name, sep="\t", assume_missing=True)
-
-# Filter out and drop missing values
-ddf = ddf.loc[~(ddf['inferred_phase'] == 'LAYOVER_DURING')]
-ddf = ddf.dropna(subset=['vehicle_id', 'time_received', 'next_scheduled_stop_distance', 'distance_along_trip'])
-#Filter line
-ddf = ddf.loc[(ddf['vehicle_id'] == 469.0) | (ddf['vehicle_id'] == 195.0)]
-
-# Get the number of unique vehicle_id values
-n_partitions = ddf['vehicle_id'].nunique().compute()
-
-# Set the index to the 'vehicle_id' column
-ddf = ddf.set_index('vehicle_id')
-
-# Repartition the data based on the index
-ddf = ddf.repartition(npartitions=n_partitions)
-
 # Define a function to sort the partition by 'time_received'
 def sort_partition(df):
     return df.sort_values('time_received')
 
-ddf = ddf.map_partitions(sort_partition)
+if __name__ == '__main__':
+    folder = "../datasets"
+    datasets = os.listdir(folder)
 
-# Apply the function to each partition
-data = ddf.map_partitions(calc_data, meta=ddf._meta).compute()
+    # Load your Dask DataFrame
+    dataset_name = os.path.join(folder, datasets[0])
+    ddf = dd.read_csv(dataset_name, sep="\t", assume_missing=True)
 
-# create an empty dataframe
-columns = ['current_stop', 'next_stop', 'distance', 'start_time', 'end_time', 'speed']
-df = pd.DataFrame(columns=columns)
+    # Filter out and drop missing values
+    ddf = ddf.query('inferred_phase != "LAYOVER_DURING"').dropna(subset=['vehicle_id', 'time_received', 'next_scheduled_stop_distance', 'distance_along_trip'])
 
-# iterate over each row in the series
-for row in data:
-    # create a temporary dataframe for the current row
-    temp_df = pd.DataFrame(row, columns=columns)
-    
-    # append the temporary dataframe to the main dataframe
-    df = df._append(temp_df, ignore_index=True)
+    #Filter line
+    ddf = ddf.loc[(ddf['vehicle_id'] == 469.0) | (ddf['vehicle_id'] == 195.0)]
 
+    # Get the number of unique vehicle_id values
+    n_partitions = ddf['vehicle_id'].nunique().compute()
 
-date = os.path.basename(dataset_name)[14:24]
+    # Set the index to the 'vehicle_id' column
+    ddf = ddf.set_index('vehicle_id')
 
-df.to_csv(f'../processed_models/{date}.csv', index=False)
+    # Repartition the data based on the index
+    ddf = ddf.repartition(npartitions=n_partitions)
+    ddf = ddf.map_partitions(sort_partition)
+
+    # Apply the function to each partition
+    data = ddf.map_partitions(calc_data, meta=ddf._meta).compute()
+
+    # create an empty dataframe
+    columns = ['current_stop', 'next_stop', 'distance', 'start_time', 'end_time', 'speed']
+    dfs = []
+
+    # iterate over each row in the series
+    for row in data:
+        dfs.append(pd.DataFrame(row, columns=columns))
+
+    df = pd.concat(dfs, ignore_index=True)
+
+    date = os.path.basename(dataset_name)[14:24]
+    df.to_csv(f'../processed_models/{date}.csv', index=False)
