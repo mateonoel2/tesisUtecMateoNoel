0df57e6e7368fc3588cf5886b807dbbe35184a1f
Mateo Noel <mateonoel@Mateos-MacBook.local>
2023-04-28 11:39:03 -0500

    calc

--- preprocess/calc_data.py
+++ preprocess/calc_data.py
@@ -31,63 +31,48 @@ def calc_data(partition):
             next_distance = next["distance_along_trip"].values[0]
             next_time = dt.strptime(next["time_received"].values[0], '%Y-%m-%d %H:%M:%S')
 
-        current_stop = current["next_scheduled_stop_id"].values[0]
-        next_stop = next["next_scheduled_stop_id"].values[0]
+        next_stop_from_current= current["next_scheduled_stop_id"].values[0]
+        next_stop_from_next = next["next_scheduled_stop_id"].values[0]
+        dist_next_stop_from_current = current["next_scheduled_stop_distance"].values[0]
+        dist_next_stop_from_next = next["next_scheduled_stop_distance"].values[0]
         dist_two_times = next_distance - current_distance
-        dist_current_next_stop = current["next_scheduled_stop_distance"].values[0]
-        next_dist_current_next_stop = next["next_scheduled_stop_distance"].values[0]
         phase = current["inferred_phase"].values[0]
-
-        if (next_distance - current_distance != dist_current_next_stop - next_dist_current_next_stop):
-            next["next_scheduled_stop_distance"].values[0] = dist_current_next_stop - (next_distance - current_distance)
-            if (next["next_scheduled_stop_distance"].values[0] < 0):
-                next["next_scheduled_stop_distance"].values[0] = 0
 
         if phase == "IN_PROGRESS":
             if is_in_progress == False:
                 first = True
-                dist_purple = None
             is_in_progress = True
         else:
             is_in_progress = False
             continue
 
-         # calculate arrival time and speed if this is the first stop on the route
-        if first:
-            if current_stop != next_stop:
-                speed = dist_two_times / (next_time - current_time).total_seconds()
-                if (speed<0.01 or speed>20):
+        if(dist_next_stop_from_next<=0):
+            continue
+
+        if (dist_two_times != dist_next_stop_from_current - dist_next_stop_from_next and next_stop_from_current==next_stop_from_next):
+            next["next_scheduled_stop_distance"].values[0] = dist_next_stop_from_current - dist_two_times
+         
+       
+        if next_stop_from_current!=next_stop_from_next:
+            speed = dist_two_times / (next_time - current_time).total_seconds()
+            if (speed<0.01 or speed>20):
                     continue
-                prev_time_to_stop = current_time + timedelta(seconds=dist_current_next_stop/speed)
+             # calculate arrival time and speed if this is the first stop on the route
+            if first:
+                prev_time_to_stop = current_time + timedelta(seconds=dist_next_stop_from_current/speed)
                 first = False
-        else:
-             # calculate arrival time and speed if this is not the first stop on the route
-            if current_stop != next_stop:
-                speed = dist_two_times / (next_time - current_time).total_seconds()
-                if speed == 0:
-                    dist_two_stops = dist_purple + next["next_scheduled_stop_distance"].values[0]
-                    continue
-                time_to_stop = min(dist_two_times, dist_current_next_stop) / speed
+            else:
+                # calculate arrival time and speed if this is not the first stop on the route
+                time_to_stop = dist_next_stop_from_current / speed
                 arrived_time = current_time + timedelta(seconds=time_to_stop)
 
-                # if the bus hasn't traveled far enough to reach the next two stops, skip to the next row
-                if dist_two_stops == 0 or prev_time_to_stop >= arrived_time:
-                    dist_two_stops = dist_purple + next["next_scheduled_stop_distance"].values[0]
-                    continue
-
-                 # calculate the speed of the bus between the last two stops
+                dist_two_stops = dist_two_times-dist_next_stop_from_current + next["next_scheduled_stop_distance"].values[0]
+                    
+                # calculate the speed of the bus between the last two stops
                 speed = dist_two_stops / (arrived_time-prev_time_to_stop).total_seconds()
-                dist_purple = None
                 
                 if speed != 0:
-                    data.append((current_stop, next_stop, dist_two_stops, speed, prev_time_to_stop.weekday(),prev_time_to_stop.date(), prev_time_to_stop.time(), arrived_time.time()))
+                    data.append((next_stop_from_current, next_stop_from_next, dist_two_stops, speed, prev_time_to_stop.weekday(),prev_time_to_stop.date(), prev_time_to_stop.time(), arrived_time.time()))
 
                 prev_time_to_stop = arrived_time
-
-        if dist_purple == None:
-            dist_purple = dist_two_times - dist_current_next_stop
-            if dist_purple < 0:
-                dist_purple = 0
-            dist_two_stops = dist_purple + next["next_scheduled_stop_distance"].values[0]
-    
     return data
