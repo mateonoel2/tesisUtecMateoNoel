5ee6beb51cad2d978d6d89faab1a2e234d899bc0
mateo <mateo.noel@utec.edu.pe>
2023-05-02 01:46:57 -0500

    ANN con spark funcional + normalizado de data

--- models/ann.py
+++ models/ann.py
@@ -1,69 +1,47 @@
-# Import necessary libraries
-import pandas as pd
-import numpy as np
-from datetime import datetime
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.callbacks import EarlyStopping
+# Import PySpark and the necessary libraries for linear regression
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
 
-# Load the data into a pandas dataframe
-data = pd.read_csv('../processed_datasets/2014-08-01.csv')
+spark = SparkSession.builder.appName("PySpark Regression ANN Example").getOrCreate()
 
-# Convert date feature to datetime format
-data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
+schema = StructType([
+    StructField("day_of_week", IntegerType(), True),
+    StructField("day_of_month", IntegerType(), True),
+    StructField("month", IntegerType(), True),
+    StructField("exit_stop", IntegerType(), True),
+    StructField("target_stop", IntegerType(), True),
+    StructField("distance", DoubleType(), True),
+    StructField("exit_time", DoubleType(), True),
+    StructField("arrive_time", DoubleType(), True)
+])
 
-# Extract additional date features
-data['day_of_week'] = data['date'].dt.dayofweek
-data['day_of_month'] = data['date'].dt.day
-data['month'] = data['date'].dt.month
+# Load data into a PySpark DataFrame
+data = spark.read.format("csv").option("header", "true").schema(schema).load("../processed_datasets/2014-08-01.csv")
 
-data = data.drop('date', axis=1)
+# Rename a column
+data = data.withColumnRenamed("arrive_time", "label")
 
-# Define a function to convert time strings to seconds since midnight
-def time_to_seconds(time_str):
-    dt = datetime.strptime(time_str, '%H:%M:%S')
-    midnight = dt.replace(hour=0, minute=0, second=0, microsecond=0)
-    return (dt - midnight).total_seconds()
+# Prepare data for linear regression training
+assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features")
+data = assembler.transform(data).select("features", "label")
 
-# Iterate over the 'exit_time' and 'arrive_time' columns and convert each value to seconds since midnight
-for col in ['exit_time', 'arrive_time']:
-    data[col] = data[col].apply(time_to_seconds)
+# Split data into training and test sets
+train_data, test_data = data.randomSplit([0.8, 0.2], seed=12345)
 
-# Concatenate the exit_stop and target_stop columns
-stops = pd.concat([data['exit_stop'], data['target_stop']], ignore_index=True)
+# Set up the linear regression model
+regressor = LinearRegression(featuresCol="features", labelCol="label")
 
-# Fit the encoder on the concatenated stops and transform the columns
-le = LabelEncoder()
-stops_encoded = le.fit_transform(stops)
-data['exit_stop'] = stops_encoded[:len(data)]
-data['target_stop'] = stops_encoded[len(data):]
+# Train the linear regression model
+model = regressor.fit(train_data)
 
-# Scale numerical variables
-scaler = MinMaxScaler()
-data[['distance', 'exit_time']] = scaler.fit_transform(data[['distance', 'exit_time']])
+# Make predictions on the test data
+predictions = model.transform(test_data)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data.drop('arrive_time', axis=1), data['arrive_time'], test_size=0.2)
+# Evaluate the model using mean squared error
+evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="mse")
+mse = evaluator.evaluate(predictions)
 
-# Define the architecture of the model
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=X_train.shape[1]))
-model.add(Dropout(0.2))
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation='linear'))
-
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# Train the model
-early_stop = EarlyStopping(monitor='val_loss', patience=10)
-model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2, callbacks=[early_stop])
-
-# Evaluate the model
-mse = model.evaluate(X_test, y_test)
-rmse = np.sqrt(mse)
-print('Root Mean Squared Error:', rmse)
+print("Mean Squared Error = %g" % mse)
---
+++ preprocess/normalize&scale.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv('../processed_datasets/2014-08-01.csv')
+
+# Convert date feature to datetime format
+data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
+
+# Extract additional date features
+data.insert(0, 'month', data['date'].dt.month)
+data.insert(0, 'day_of_month', data['date'].dt.day)
+data.insert(0, 'day_of_week', data['date'].dt.dayofweek)
+
+data = data.drop('date', axis=1)
+
+# Define a function to convert time strings to seconds since midnight
+def time_to_seconds(time_str):
+    dt = datetime.strptime(time_str, '%H:%M:%S')
+    midnight = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+    return (dt - midnight).total_seconds()
+
+# Iterate over the 'exit_time' and 'arrive_time' columns and convert each value to seconds since midnight
+for col in ['exit_time', 'arrive_time']:
+    data[col] = data[col].apply(time_to_seconds)
+# Concatenate the exit_stop and target_stop columns
+stops = pd.concat([data['exit_stop'], data['target_stop']], ignore_index=True)
+
+# Fit the encoder on the concatenated stops and transform the columns
+le = LabelEncoder()
+stops_encoded = le.fit_transform(stops)
+data['exit_stop'] = stops_encoded[:len(data)]
+data['target_stop'] = stops_encoded[len(data):]
+
+# Scale numerical variables
+scaler = MinMaxScaler()
+data['distance'] = scaler.fit_transform(data[['distance']])
+
+# Divide 'exit_time' and 'arrive_time' columns by 86400 to normalize to [0, 1] range
+data['exit_time'] = data['exit_time'] / 86400
+data['arrive_time'] = data['arrive_time'] / 86400
+
+data.to_csv(f'../processed_datasets/2014-08-01.csv', index=False)
