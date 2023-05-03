5cf0bf501aeb2bd3041adbd241cfb742017ca0b4
mateo <mateo.noel@utec.edu.pe>
2023-05-03 18:12:12 -0500

    fix

--- preprocess/dask_preprocess.py
+++ preprocess/dask_preprocess.py
@@ -1,7 +1,11 @@
 import dask.dataframe as dd
 import os
-from process_data import process_data
-from dask.distributed import Client, wait
+from normalize import normalize
+from dist_fix import dist_fix
+from sort_and_calc import sort_and_calc
+import pandas as pd
+import pyarrow as pa
+from dask.distributed import Client
 import dask.bag as db
 
 if __name__ == '__main__':    
@@ -15,18 +19,46 @@ if __name__ == '__main__':
     for dataset in datasets:
         dataset_names.append(os.path.join(folder, dataset))
     
-    # use a wait condition to wait for an available client
-    while not client:
-        client = Client()
-        wait([client])
-
     # Create a Dask bag of Dask dataframes
     dataframes_bag = db.from_sequence(dataset_names).map(lambda fn: dd.read_csv(fn, sep="\t", assume_missing=True, usecols=['time_received', 'vehicle_id', 'distance_along_trip', 'inferred_phase', 'next_scheduled_stop_distance', 'next_scheduled_stop_id']))
     
-    # use a wait condition to wait for an available client
-    while not client:
-        client = Client()
-        wait([client])
+    def process_data(ddf):
+        # Get the first value of the time_received column
+        first_time_received = ddf['time_received'].head(1).values[0]
+        date = first_time_received[0:10]
+
+        # Filter out all phases that aren't LAYOVER_DURING and all rows with null
+        ddf = ddf.loc[(ddf['inferred_phase'] != "LAYOVER_DURING")].dropna()
+
+        # Apply the sort_and_calc() function to each group separately
+        group = ddf.groupby('vehicle_id')
+        ddf = group.apply(sort_and_calc, meta=pd.DataFrame(columns=['trip', 'distance', 'date', 'exit_time', 'arrive_time']))
+        ddf = ddf.reset_index(drop=True)
+
+        # Apply the dist_fix() function to each group separately
+        group = ddf.groupby('trip')
+        ddf = group.apply(dist_fix, meta=ddf._meta)
+        ddf = ddf.reset_index(drop=True)
+
+        ddf = ddf.map_partitions(normalize, meta=pd.DataFrame(columns=['exit_stop', 'target_stop', 'day_of_week', 'day_of_month', 'month','distance', 'exit_time', 'arrive_time']))
+        
+        partition_schema = pa.schema([
+            pa.field('exit_stop', pa.int64()),
+            pa.field('target_stop', pa.int64()),
+            pa.field('day_of_week', pa.int32()),
+            pa.field('day_of_month', pa.int32()),
+            pa.field('month', pa.int32()),
+            pa.field('distance', pa.float64()),
+            pa.field('exit_time', pa.float64()),
+            pa.field('arrive_time', pa.float64()),
+            pa.field('__null_dask_index__', pa.int64())
+        ])
+
+        ddf.to_parquet(f'../processed_datasets/{date}.parquet', engine='pyarrow', schema=partition_schema)
+
+        print(f"Finished processing {date} dataset.")
+        
+        return None
 
     dataframes_bag.map(process_data).compute()
 
