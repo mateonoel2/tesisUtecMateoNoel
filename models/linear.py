c39f20b5fefd0d7f493f08a9d0a1f1f40899fb14
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-04 18:49:15 -0500

    woking model

--- models/linear.py
+++ models/linear.py
@@ -1,10 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.sql.functions import udf
-from datetime import datetime, timedelta
-import os
 import sys
 
 # Define a function to handle errors during model training
@@ -17,58 +13,16 @@ if __name__ == '__main__':
         # Create a SparkSession
         spark = SparkSession.builder \
         .appName("PySpark Linear Regression") \
-        .config("spark.local.dir", "./tmp") \
+        .config("spark.driver.memory", "20g") \
+        .config("spark.executor.memory", "90g") \
+        .config("spark.executor.instances", "1") \
+        .config("spark.executor.cores", "79") \
         .config("spark.driver.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
         .config("spark.executor.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
         .getOrCreate()
-
-        N = 92
-        start_date = datetime(2014, 8, 1)
-        end_date = start_date + timedelta(days=N-1)
-
-        date_range = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
-        date_range_str = [date.strftime("%Y-%m-%d") for date in date_range]
-
-        path = "../processed_datasets/{date}.parquet"
-
-        valid_paths = []
-        for date_str in date_range_str:
-            file_path = path.format(date=date_str)
-            if os.path.exists(file_path):
-                file_size = os.path.getsize(file_path)
-                if file_size > 0:
-                    valid_paths.append(file_path)
-
-
-        print("Number of days to predict:", len(valid_paths))
         
         # Load data from Parquet
-        data = spark.read.format("parquet").load(valid_paths)
-        data = data.coalesce(1)
-        data = data.drop('__null_dask_index__').dropna().dropDuplicates()
-
-        # Get the unique values from exit_stop column
-        exit_stops = data.select("exit_stop").distinct().rdd.flatMap(lambda x: x).collect()
-        target_stops = data.select("target_stop").distinct().rdd.flatMap(lambda x: x).collect()
-
-        all_stops = list(set(exit_stops + target_stops))
-        # Create a mapping from the old values to the new values
-        mapping = dict(zip(all_stops, range(1, len(all_stops) + 1)))
-        # Create UDFs to apply the mapping to each column
-        exit_stop_mapping = udf(lambda exit_stop: mapping[exit_stop])
-        target_stop_mapping = udf(lambda target_stop: mapping[target_stop])
-
-        # Apply the mapping to the DataFrame using withColumn
-        data = data \
-            .withColumn("exit_stop", exit_stop_mapping(data["exit_stop"]).cast("integer")) \
-            .withColumn("target_stop", target_stop_mapping(data["target_stop"]).cast("integer"))
-        
-        # Rename a column
-        data = data.withColumnRenamed("arrive_time", "label")
-
-        # Prepare data for linear regression training
-        assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features")
-        data = assembler.transform(data).select("features", "label")
+        data = spark.read.parquet("../processed_datasets2")
 
         # Split data into training and test sets
         train_data, test_data = data.randomSplit([0.8, 0.2], seed=21)
---
+++ preprocess/spark_preprocess.py
@@ -0,0 +1,78 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from datetime import datetime, timedelta
+from pyspark.ml.feature import VectorAssembler
+import os
+import sys
+
+# Define a function to handle errors
+def handle_error(e):
+    print("Error: ", e)
+    sys.exit(1)
+
+if __name__ == '__main__':    
+    try:
+        # Create a SparkSession
+        spark = SparkSession.builder \
+        .appName("PySpark preprocess") \
+        .config("spark.driver.memory", "20g") \
+        .config("spark.executor.memory", "90g") \
+        .config("spark.executor.instances", "10") \
+        .config("spark.executor.cores", "7") \
+        .config("spark.driver.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
+        .config("spark.executor.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
+        .getOrCreate()
+
+        N = 92
+        start_date = datetime(2014, 8, 1)
+        end_date = start_date + timedelta(days=N-1)
+
+        date_range = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
+        date_range_str = [date.strftime("%Y-%m-%d") for date in date_range]
+
+        path = "../processed_datasets/{date}.parquet"
+
+        valid_paths = []
+        for date_str in date_range_str:
+            file_path = path.format(date=date_str)
+            if os.path.exists(file_path):
+                file_size = os.path.getsize(file_path)
+                if file_size > 0:
+                    valid_paths.append(file_path)
+        
+        # Load data from Parquet
+        data = spark.read.format("parquet").load(valid_paths)
+        data = data.coalesce(10)
+        data = data.drop('__null_dask_index__').dropna().dropDuplicates().coalesce(10)
+
+        # Get the unique values from exit_stop column
+        exit_stops = data.select("exit_stop").distinct().rdd.flatMap(lambda x: x).collect()
+        target_stops = data.select("target_stop").distinct().rdd.flatMap(lambda x: x).collect()
+
+        all_stops = list(set(exit_stops + target_stops))
+        # Create a mapping from the old values to the new values
+        mapping = dict(zip(all_stops, range(1, len(all_stops) + 1)))
+        # Create UDFs to apply the mapping to each column
+        exit_stop_mapping = udf(lambda exit_stop: mapping[exit_stop])
+        target_stop_mapping = udf(lambda target_stop: mapping[target_stop])
+
+        # Apply the mapping to the DataFrame using withColumn
+        data = data \
+            .withColumn("exit_stop", exit_stop_mapping(data["exit_stop"]).cast("integer")) \
+            .withColumn("target_stop", target_stop_mapping(data["target_stop"]).cast("integer"))
+        
+        # Rename a column
+        data = data.withColumnRenamed("arrive_time", "label")
+
+        # Prepare data for regression training
+        assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features")
+        data = assembler.transform(data).select("features", "label")
+
+        data = data.coalesce(1)
+
+        data.write.format("parquet").mode('overwrite').save("../processed_datasets2")
+
+    except Exception as e:
+        handle_error(e)
+    finally:
+        spark.stop()
