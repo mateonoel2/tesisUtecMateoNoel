86086af5a0b63b65cba8944e6f4d80a47a16afd7
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-04 03:45:37 -0500

    FUNCIONA

--- models/linear.py
+++ models/linear.py
@@ -1,44 +1,64 @@
-# Import PySpark and the necessary libraries for linear regression
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
+import os
+import sys
 
-spark = SparkSession.builder.appName("PySpark Regression ANN Example").getOrCreate()
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--conf spark.driver.extraJavaOptions="-XX:-UseGCOverheadLimit" pyspark-shell'
 
-# Load data into a PySpark DataFrame using the specified schema
+# Define a function to handle errors during model training
+def handle_error(e):
+    print("Error occurred during model training: ", e)
+    sys.exit(1)
 
-data = spark.read.format("parquet").load("../processed_datasets/*.parquet")
-data = data.drop('__null_dask_index__')
-data = data.dropDuplicates()
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("PySpark Linear Regression") \
+    .config("spark.network.timeout", "2h") \
+    .config("spark.core.connection.ack.wait.timeout", "2h") \
+    .config("spark.executor.memory", "100g") \
+    .config("spark.driver.memory", "10g") \
+    .config("spark.executor.instances", "40") \
+    .config("spark.executor.cores", "2") \
+    .getOrCreate()
 
-print("Number of rows:", data.count())
+try:
+    # Load data from Apache Parquet
+    data = spark.read.format("parquet").load("../processed_datasets/*.parquet").limit(80000000)
+    data = data.drop('__null_dask_index__').dropna().dropDuplicates()
 
-# Rename a column
-data = data.withColumnRenamed("arrive_time", "label")
+    print("Number of rows:", data.count())
 
-# Prepare data for linear regression training
-assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features")
-data = assembler.transform(data).select("features", "label")
+    # Rename a column
+    data = data.withColumnRenamed("arrive_time", "label")
 
-# Split data into training and test sets
-train_data, test_data = data.randomSplit([0.8, 0.2], seed=21)
+    # Prepare data for linear regression training
+    assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features")
+    data = assembler.transform(data).select("features", "label")
 
-# Set up the linear regression model
-regressor = LinearRegression(featuresCol="features", labelCol="label")
+    # Split data into training and test sets
+    train_data, test_data = data.randomSplit([0.8, 0.2], seed=21)
 
-# Train the linear regression model
-model = regressor.fit(train_data)
+    # Set up the linear regression model
+    regressor = LinearRegression(featuresCol="features", labelCol="label")
 
-# Make predictions on the test data
-predictions = model.transform(test_data)
+    # Train the linear regression model
+    model = regressor.fit(train_data)
 
-# Evaluate the model using mean squared error and r2
-evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="rmse")
-rmse = evaluator.evaluate(predictions)
-print("Root Mean Squared Error (RMSE) = {:.4f}".format(rmse))
+    # Make predictions on the test data
+    predictions = model.transform(test_data)
 
-evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="r2")
-r2 = evaluator.evaluate(predictions)
-print("R-squared (R2) = {:.4f}".format(r2))
+    # Evaluate the model using mean squared error and r2
+    evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="rmse")
+    rmse = evaluator.evaluate(predictions)
+    print("Root Mean Squared Error (RMSE) = {:.4f}".format(rmse))
 
+    evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="r2")
+    r2 = evaluator.evaluate(predictions)
+    print("R-squared (R2) = {:.4f}".format(r2))
+
+except Exception as e:
+    handle_error(e)
+finally:
+    spark.stop()
