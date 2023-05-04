b8d54f7e18a0b8cd5d0de572937f823c741e0eec
Roberto Furukawa <rfurukawa@khipu.utec.edu.pe>
2023-05-03 23:48:55 -0500

    update

--- preprocess/normalize.py
+++ preprocess/normalize.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datetime import datetime
 
+def time_to_seconds(time_obj):
+    midnight = datetime.combine(datetime.today(), datetime.min.time())
+    return (datetime.combine(datetime.today(), time_obj) - midnight).total_seconds()
+
 def normalize(data):
     # Convert date feature to datetime format
     data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
@@ -12,16 +16,13 @@ def normalize(data):
 
     data = data.drop('date', axis=1)
 
-    # Define a function to convert time strings to seconds since midnight
-    def time_to_seconds(time_str):
-        time_str = time_str.strftime('%H:%M:%S')
-        dt = datetime.strptime(time_str, '%H:%M:%S')
-        midnight = dt.replace(hour=0, minute=0, second=0, microsecond=0)
-        return (dt - midnight).total_seconds()
+    # Convert datetime to seconds since midnight
+    data['exit_time'] = data['exit_time'].apply(time_to_seconds)
+    data['arrive_time'] = data['arrive_time'].apply(time_to_seconds)
 
-    # Iterate over the 'exit_time' and 'arrive_time' columns and convert each value to seconds since midnight
-    for col in ['exit_time', 'arrive_time']:
-        data[col] = data[col].apply(time_to_seconds)
+    # Divide 'exit_time' and 'arrive_time' columns by 86400 to normalize to [0, 1] range
+    data['exit_time'] = data['exit_time'] / 86400
+    data['arrive_time'] = data['arrive_time'] / 86400
 
     data.insert(0, 'target_stop', '')   
     data.insert(0, 'exit_stop', '')
@@ -34,9 +35,7 @@ def normalize(data):
 
     data = data.drop('trip', axis=1)
 
-    # Divide 'exit_time' and 'arrive_time' columns by 86400 to normalize to [0, 1] range
-    data['exit_time'] = data['exit_time'] / 86400
-    data['arrive_time'] = data['arrive_time'] / 86400
+
     data['distance'] = data[['distance']] / 5000
 
     return data
