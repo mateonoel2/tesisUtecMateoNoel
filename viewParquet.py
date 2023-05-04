58e36b9d9a8a4b497a52b40ad0155270f4fcf3c9
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-04 00:45:19 -0500

    clean up

--- viewParquet.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the parquet file into a pandas dataframe
-df = pd.read_parquet('processed_datasets/2014-08-01.parquet')
-
-# Save the dataframe as a CSV file
-df.to_csv('processed_datasets/test.csv', index=False)
