d2a326e0efe0a4d94788fbd492e11ba89d2336c9
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-12 19:52:07 -0500

    check

--- preprocess_test/dask_preprocess.py
+++ preprocess_test/dask_preprocess.py
@@ -1,8 +1,7 @@
 import dask.dataframe as dd
 import os
 from process_data import process_data
-from dask.distributed import Client, wait
-import time
+from dask.distributed import Client
 import dask.config
 
 dask.config.set({'distributed.comm.timeouts.connect': '2h'})
@@ -12,28 +11,13 @@ if __name__ == '__main__':
     client = Client(timeout="2h", n_workers=80, threads_per_worker=1, memory_limit=None)
 
     folder = "../datasets"
-    datasets = os.listdir(folder)
-
-    num_datasets = len(datasets)
+    datasets = os.path.join(folder, "*.txt")
     
-    results = []
-    
-    for i in range(0, num_datasets):
-        dataset_name = os.path.join(folder, datasets[i]) 
-
-        # Create a Dask dataframe from the CSV file
-        ddf = dd.read_csv(dataset_name, sep="\t", assume_missing=True,
-                        usecols=['time_received', 'vehicle_id', 'distance_along_trip', 'inferred_phase',
-                                  'next_scheduled_stop_distance', 'next_scheduled_stop_id']
-                        )
-                
-        # submit the computation to the cluster
-        future = client.submit(process_data, ddf, priority=num_datasets-i-1)
-        results.append(future)
-        time.sleep(10)
-
-    #wait for the results  
-    wait(results)
+    # Create a Dask dataframe from the CSV file
+    ddf = dd.read_csv(datasets, sep="\t", assume_missing=True,
+                    usecols=['time_received', 'vehicle_id', 'distance_along_trip', 'inferred_phase',
+                                'next_scheduled_stop_distance', 'next_scheduled_stop_id'])
+            
+    process_data(ddf)
 
     print("SUCCESS")
-    client.close()
