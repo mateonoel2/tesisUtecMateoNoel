c472db6aba1ba1bc34d457896d0f4e2c7774a433
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-12 19:53:49 -0500

    Testing con v_id 371

--- parquet2Viewer.py
+++ parquet2Viewer.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     .appName("parquet2 viewer") \
     .getOrCreate()
 
-    data = spark.read.format("parquet").load("processed_datasets2/dataset").limit(1000)
+    data = spark.read.format("parquet").load("processed_datasets2/dataset")\
+           .filter(col("vehicle_id") == 371)\
+           .limit(1000)
 
     data.show(1000)
