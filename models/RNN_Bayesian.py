ff519c44e11decd7335eeb86e5227b6df0b29229
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-17 21:23:48 -0500

    a

--- models/RNN_Bayesian.py
+++ models/RNN_Bayesian.py
@@ -1,5 +1,3 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import abs, asc, col
 import sys
 import pandas as pd
 import numpy as np
@@ -15,69 +13,45 @@ def handle_error(e):
     print("Error occurred during model training: ", e)
     sys.exit(1)
 
-
 def train_evaluate_model(epochs, batch_size):
     try:
-        rmse_scores = []
-        dataset_sizes = [int(56839426/89), int(56839426/45), int(56839426/22), int(56839426/11), int(56839426/5), int(56839426/2), int(56839426)]
+        data = data = pd.read_parquet("../processed_datasets_concat/dataset")
 
-        for dataset_size in dataset_sizes: 
-            # Load data from Parquet
-            data = spark.read.parquet("../processed_datasets3/dataset").coalesce(1)\
-                .limit(dataset_size)\
-                .orderBy(asc("month"), asc("day_of_month"), asc("exit_time")).cache()
+        # Perform one-hot encoding for categorical features
+        categorical_features = ['day_of_week', 'exit_stop', 'target_stop']
+        encoded_features = pd.get_dummies(data[categorical_features])
+        numerical_features = data[['distance', 'exit_time']]
+        features = pd.concat([encoded_features, numerical_features], axis=1).values
 
-            data = data.toPandas()
+        labels = data['label'].values
 
-            # Perform one-hot encoding for categorical features
-            categorical_features = ['day_of_week', 'exit_stop', 'target_stop']
-            encoded_features = pd.get_dummies(data[categorical_features])
-            numerical_features = data[['distance', 'exit_time']]
-            features = pd.concat([encoded_features, numerical_features], axis=1).values
+        # Split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.1, shuffle=False)
 
-            labels = data['label'].values
+        # Reshape the input data to match the LSTM input shape (samples, timesteps, features)
+        X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+        X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
-            # Split the data into training and testing sets
-            X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.1, shuffle=False)
+        # Define the model
+        model = Sequential()
+        model.add(LSTM(128, input_shape=(1, features.shape[1])))
+        model.add(Dense(1))
 
-            # Reshape the input data to match the LSTM input shape (samples, timesteps, features)
-            X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
-            X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+        # Compile the model
+        model.compile(loss='mean_squared_error', optimizer='adam')
 
-            # Define the model
-            model = Sequential()
-            model.add(LSTM(128, input_shape=(1, features.shape[1])))
-            model.add(Dense(1))
+        # Train the model
+        model.fit(X_train, y_train, epochs=int(epochs), batch_size=int(batch_size), verbose=0)
 
-            # Compile the model
-            model.compile(loss='mean_squared_error', optimizer='adam')
-
-            # Train the model
-            model.fit(X_train, y_train, epochs=int(epochs), batch_size=int(batch_size), verbose=0)
-
-            # Evaluate the model
-            y_pred = model.predict(X_test)
-            rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-            rmse_scores.append(rmse)
-
-        average_rmse = np.mean(rmse_scores)
-        return -average_rmse 
+        # Evaluate the model
+        y_pred = model.predict(X_test)
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        
+        return -rmse
 
     except Exception as e:
         handle_error(e)
-    finally:
-        spark.stop()
 
-
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("PySpark RNN LSTM") \
-    .config("spark.executor.instances", "10") \
-    .config("spark.executor.cores", "8") \
-    .config("spark.driver.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
-    .config("spark.executor.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
-    .getOrCreate()
-        
 # Define the parameter ranges for Bayesian optimization
 pbounds = {
     'epochs': (10, 100),
